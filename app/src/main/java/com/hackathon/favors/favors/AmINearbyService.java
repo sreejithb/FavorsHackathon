24444ec80dec4f70417d7570bd32b5c981f8de7f
sreejithb <sreejithb@gmail.com>
2016-09-04 00:27:52 +0800

    AfterNotification

--- app/src/main/java/com/hackathon/favors/favors/AmINearbyService.java
+++ app/src/main/java/com/hackathon/favors/favors/AmINearbyService.java
@@ -1,5 +1,8 @@
 package com.hackathon.favors.favors;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -14,54 +17,31 @@ import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
 import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.hackathon.favors.myapplication.backend.myApi.MyApi;
+import com.hackathon.favors.myapplication.backend.myApi.model.GroceryListWithRating;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by sreejith on 3/9/16.
  */
-public class AmINearbyService extends Service {
+public class AmINearbyService extends Service implements AsyncResponse {
     String deb = "AMI";
+    static int hasfound= 0;
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         super.onStartCommand(intent, flags, startId);
         Log.d(deb, "started");
-        MyApi myApiService = null;
-        int res = 0;
-        while (res != 1) {
-
-
-            if (myApiService == null) {  // Only do this once
-                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
-                        new AndroidJsonFactory(), null)
-                        // options for running against local devappserver
-                        // - 10.0.2.2 is localhost's IP address in Android emulator
-                        // - turn off compression when running against local devappserver
-                        .setRootUrl("https://favorsbackend.appspot.com/_ah/api/")
-                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                            @Override
-                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-                                abstractGoogleClientRequest.setDisableGZipContent(true);
-                            }
-                        });
-
-                myApiService = builder.build();
-            }
-
-            try {
-                res = Integer.parseInt(myApiService.amINearby("test").execute().getData());
-            } catch (IOException e) {
-                e.getMessage();
-            }
-            if (res == 1) {
-                Log.d(deb, "GotOne");
-            }
-
-            SystemClock.sleep(1000);
-
-        }
+        runCheck();
         return 1;
+    }
+
+    private void runCheck() {
+        String[] subparams = {FunctionDirectory.AMINEARBY,"Mary"};
+        final CallBackEndTask callBackEndTask = new CallBackEndTask();
+        callBackEndTask.setAsyncResponse(this);
+        callBackEndTask.execute(new Pair<Context, String[]>(this, subparams));
 
     }
 
@@ -69,5 +49,41 @@ public class AmINearbyService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    @Override
+    public void processResult(String result) {
+        Log.d(deb,result);
+        if (Integer.parseInt(result) == 1){
+            Intent intent = new Intent(this, MainActivity.class);
+// use System.currentTimeMillis() to have a unique ID for the pending intent
+            PendingIntent pIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent, 0);
+
+// build notification
+// the addAction re-use the same intent to keep the example short
+            Notification n  = new Notification.Builder(this)
+                    .setContentTitle("People could use your help")
+                    .setSmallIcon(R.drawable.icon)
+                    .setContentIntent(pIntent)
+                    .setAutoCancel(true).build();
+            NotificationManager notificationManager =
+                    (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+            notificationManager.notify(0, n);
+        }else {
+            SystemClock.sleep(1000);
+            runCheck();
+        }
+    }
+
+
+    @Override
+    public void processResultList(List<GroceryListWithRating> gListResult) {
+        Log.d(deb,"herer1");
+    }
+
+    @Override
+    public void processResultItem(GroceryListWithRating groceryListResult) {
+        Log.d(deb,"here2");
     }
 }
