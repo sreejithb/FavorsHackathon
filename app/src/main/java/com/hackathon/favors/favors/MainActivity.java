43e274395f49c091c8f31a2e72963f61c825dbd0
sreejithb <sreejithb@gmail.com>
2016-09-03 16:23:22 +0800

    Ignore

---
+++ app/src/main/java/com/hackathon/favors/favors/MainActivity.java
@@ -0,0 +1,23 @@
+package com.hackathon.favors.favors;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import com.facebook.FacebookSdk;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        EditText editText= (EditText) findViewById(R.id.hackathonet);
+        editText.setText("THON");
+
+    }
+}
